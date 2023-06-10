9203987873b86c67c2fa0749458ddc76bee71f32
JULIEN MASNADA <rostskadat@users.noreply.github.com>
2023-06-10 08:26:32 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -48,9 +48,9 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadlessNoSandbox'],
     customLaunchers: {
-      ChromeHeadlessCI: {
+      ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox']
       }
